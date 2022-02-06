22517536aa2a626f83b9775756b5268004b22ca0
myhency <hency.yeo@gmail.com>
2022-02-06 18:23:51 +0900

    NFT-20 로그인 테스트 코드 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,11 +5,13 @@ import { AppModule } from './../src/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exception/http-exception.filter';
 import express from 'express';
+import { personalSign } from 'eth-sig-util';
+import { bufferToHex } from 'ethereumjs-util';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -39,18 +41,17 @@ describe('AppController (e2e)', () => {
   });
 
   describe('Auth Test', () => {
-    it('should return 200 when /auth?accountAddress=test (GET) called', () => {
+    it('should return 200 when /auth?accountAddress=`accountAddress` (GET) called', async () => {
       // Given
-      const URL = '/auth?accountAddress=test';
+      const URL =
+        '/auth?accountAddress=0x264D6BF791f6Be6F001A95e895AE0a904732d473';
 
       // When
-      return request
+      const res = await request
         .default(app.getHttpServer())
         .get(URL)
-        .expect(200)
-        .then((res) => {
-          expect(res.body).toHaveProperty('nonce');
-        });
+        .expect(200);
+      expect(res.body).toHaveProperty('nonce');
     });
 
     it('should return 400 when /auth (POST) called without accountAddress and signature', () => {
@@ -66,36 +67,38 @@ describe('AppController (e2e)', () => {
         .expect(400);
     });
 
-    it.todo(
-      'should return 403 when /auth (POST) called with unmatched accountAddress and nonce',
-    );
+    it('should return 201 when /auth (POST) called with matched accountAddress and nonce', async () => {
+      // Given
+      const accountAddress = '0x5E4E12042cbe7EFCFcCd235265b2a8b190b5Fd5A';
+      const privateKey =
+        '84aee955437a675c564e0e481efa3fbd1859795161e936d82e75010cc61e0e1d';
+      const URLForGettingNonce = `/auth?accountAddress=${accountAddress}`;
+      const URL = '/auth';
+      const data = {
+        accountAddress,
+      };
+      const res = await request
+        .default(app.getHttpServer())
+        .get(URLForGettingNonce)
+        .expect(200);
+      const pk = Buffer.from(privateKey, 'hex');
+      const msg = `I am signing my one-time nonce: ${res.body.nonce}`;
+      const msgBufferHex = bufferToHex(Buffer.from(msg, 'utf8'));
+      const sig = personalSign(pk, { data: msgBufferHex });
 
-    // it('should return 403 when /auth (POST) called with unmatched accountAddress and nonce', async (done) => {
-    //   // Given
-    //   const URLForGettingNonce =
-    //     '/auth?accountAddress=0x264D6BF791f6Be6F001A95e895AE0a904732d473';
-    //   const URL = '/auth';
-    //   const data = {
-    //     accountAddress: '0x264D6BF791f6Be6F001A95e895AE0a904732d473',
-    //     signature:
-    //       '0xc5f30a1b7b9a036f8e92b8f4105129bdc29520c6d22f04a1c9e474b47a2c5ead35f2027143eb932cde364f9cc9259fe268afa94f947ce31e8082180a55120fe01b',
-    //     nonce: '',
-    //   };
-
-    //   const nonceResp = await request
-    //     .default(app.getHttpServer())
-    //     .get(URLForGettingNonce)
-    //     .expect(200);
-    //   const createAccountResp = await request
-    //     .default(app.getHttpServer())
-    //     .post(URL)
-    //     .send({
-    //       ...data,
-    //       nonce: nonceResp.body.nonce,
-    //     })
-    //     .expect(403);
-
-    //   done();
-    // });
+      // When
+      return await request
+        .default(app.getHttpServer())
+        .post(URL)
+        .send({
+          ...data,
+          signature: sig,
+          nonce: res.body.nonce,
+        })
+        .expect(201)
+        .expect((res) => {
+          expect(JSON.parse(res.text)).toHaveProperty('accessToken');
+        });
+    });
   });
 });
